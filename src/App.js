fb4dadd1f99386e0e1774b5046955000ebd67959
Anastasiia-deign <neveykina.nastya@gmail.com>
2023-03-22 01:13:00 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.instagram.com/neveykina_nastya" target="_blank">
+          {" "}
+          Anastasiia Tatarchenko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Anastasiia-deign/react_my_best_weather"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
- 
