b35a2489c7795f1f987229fc0d17a282d9270601
Anastasiia-deign <neveykina.nastya@gmail.com>
2023-03-22 02:01:39 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.instagram.com/neveykina_nastya" target="_blank">
-          {" "}
-          Anastasiia Tatarchenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Anastasiia-deign/react_my_best_weather"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="conteiner">
+        <h1> Weather App</h1>
+        <button className="btn btn-primary"></button>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.instagram.com/neveykina_nastya" target="_blank">
+            {" "}
+            Anastasiia Tatarchenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Anastasiia-deign/react_my_best_weather"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
