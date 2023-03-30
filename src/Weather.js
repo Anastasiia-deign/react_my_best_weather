45d8c7ec577ba017961cae7e06e6aad6d0f7ffbf
Anastasiia-deign <neveykina.nastya@gmail.com>
2023-03-30 14:56:12 +0200

    HW 6

--- src/Weather.js
+++ src/Weather.js
@@ -10,15 +10,6 @@ export default function Weather(props) {
 
   function handleResponse(response) {
     setWeatherData({
-      // ready: true,
-      // coordinates: response.data.coordinates,
-      // temperature: response.data.temperature.current,
-      // humidity: response.data.temperature.humidity,
-      // date: new Date(response.data.time * 1000),
-      // description: response.data.condition.description,
-      // icon: response.data.condition.icon,
-      // wind: response.data.wind.speed,
-      // city: response.data.city,
       ready: true,
       temperature: response.data.main.temp,
       coordinates: response.data.coord,
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -22,8 +22,8 @@ export default function WeatherForecastDay(props) {
   }
 
   return (
-    <div>
-      <div className="WeatherForecastDay">{day()}</div>
+    <div className="WeatherForecastDay">
+      <div className="forecast-time">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
